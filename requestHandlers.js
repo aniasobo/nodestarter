b22f631dede1054bf9ef86fc5714b4a871dd7697
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-08-29 19:09:57 +0100

    Add request handlers

---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+function start() {
+  console.log("Request handler START was called");
+}
+
+function upload() {
+  console.log("Request handler UPLOAD was called")
+}
+
+exports.start = start;
+exports.upload = upload;
