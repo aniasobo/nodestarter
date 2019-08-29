02bdc35b0d090b50062f645a77161c002ca1b855
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-08-29 18:33:53 +0100

    Server.js is now a module

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var http = require("http"); // this module ships with Node.js
 
-function onRequest(request, response) { // req and resp are objects with methods
-  console.log("Request received.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}
+function start() {
+  function onRequest(request, response) { // req and resp are objects with methods
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
 http.createServer(onRequest).listen(8888);
-
 console.log("Server has started.")
+}
+
+exports.start = start;
